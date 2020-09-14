efa744f2658907f0a139cb0f55c2159b3033feea
paraskasrija <paraskasrija4@gmail.com>
2020-09-14 20:41:02 +0530

    Firebase

--- src/screens/Dashboard/index.js
+++ src/screens/Dashboard/index.js
@@ -1,24 +1,55 @@
 
-import { View,Text} from 'react-native';
+import { View,Text, BackHandler} from 'react-native';
 import { TextInput, TouchableOpacity } from 'react-native-gesture-handler';
 import React, { Component } from 'react';
+import {database,firebase} from '@react-native-firebase/database';
 
-
+// const reference = database().ref('/users/123');
+// const usersCollection = firestore().collection('Users');
 
 
 
 class Dashboard extends Component {
 
-  
-
+    constructor(prop){
+        super(prop);
+        this.state = ({
+            name : '' ,
+            lastName : ''
+        })
+    };
+    writeUserData(name,lastName){
+        firebase.database().ref('Users/').set({
+           name : "dfdf",
+           lastName: "dfsd"
+        }).then((data)=>{
+            //success callback
+            console.log('data ' , data)
+        }).catch((error)=>{
+            //error callback
+            console.log('error ' , error)
+        })
+    }
 
     render(){
        
         return(
             <View style={{flex:1,justifyContent:'center',marginHorizontal:10}}>
-             <Text>
-                 paras
-             </Text>
+             <TextInput
+             placeholder='enter name'
+             onchangeText = {(name) => this.setState({name})}
+             />
+              <TextInput
+             placeholder='enter lastname'
+             onchangeText = {(lastName) => this.setState({lastName})}
+             />
+             <View>
+                 <TouchableOpacity style={{backgroundColor:'blue'}} onPress = {() => this.writeUserData(this.state.name,this.state.lastName)}>
+                     <Text style={{textAlign:'center'}}>
+                         on
+                     </Text>
+                 </TouchableOpacity>
+             </View>
             </View>
         );
     }
